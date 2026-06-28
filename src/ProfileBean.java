bfa43b1dc99c4a955e3118c2cdd561388983fa18
en-surobe <135934395+en-surobe@users.noreply.github.com>
2026-06-28 20:21:17 +0800

    Refactor ProfileBean class and remove Serializable

--- src/ProfileBean.java
+++ src/ProfileBean.java
@@ -1,67 +1,19 @@
-import java.io.Serializable;
+package com.profile;
 
-public class ProfileBean implements Serializable {
+public class ProfileBean {
+    private String name, studentId, program, email, hobbies, introduction;
 
-    private static final long serialVersionUID = 1L;
+    public String getName() { return name; }
+    public String getStudentId() { return studentId; }
+    public String getProgram() { return program; }
+    public String getEmail() { return email; }
+    public String getHobbies() { return hobbies; }
+    public String getIntroduction() { return introduction; }
 
-    private String studentId;
-    private String name;
-    private String program;
-    private String email;
-    private String hobbies;
-    private String introduction;
-
-    // Default constructor
-    public ProfileBean() {
-    }
-
-    // Getters and Setters
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getProgram() {
-        return program;
-    }
-
-    public void setProgram(String program) {
-        this.program = program;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getHobbies() {
-        return hobbies;
-    }
-
-    public void setHobbies(String hobbies) {
-        this.hobbies = hobbies;
-    }
-
-    public String getIntroduction() {
-        return introduction;
-    }
-
-    public void setIntroduction(String introduction) {
-        this.introduction = introduction;
-    }
+    public void setName(String name) { this.name = name; }
+    public void setStudentId(String studentId) { this.studentId = studentId; }
+    public void setProgram(String program) { this.program = program; }
+    public void setEmail(String email) { this.email = email; }
+    public void setHobbies(String hobbies) { this.hobbies = hobbies; }
+    public void setIntroduction(String introduction) { this.introduction = introduction; }
 }
