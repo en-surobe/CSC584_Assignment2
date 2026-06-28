16a155b4e38ac5f51a8645accf969eaf0bef9ed0
en-surobe <135934395+en-surobe@users.noreply.github.com>
2026-06-28 19:39:12 +0800

    Delete ProfileServlet.java

--- ProfileServlet.java
+++
@@ -1,8 +0,0 @@
-ProfileBean profile = new ProfileBean();
-
-profile.setName(name);
-profile.setStudentId(studentId);
-profile.setProgram(program);
-profile.setEmail(email);
-profile.setHobbies(hobbies);
-profile.setIntroduction(introduction);
